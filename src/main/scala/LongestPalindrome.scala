9057fc756c45770f23cd658cb691ab55c7c04b19
reminia <reminia.scarlet@gmail.com>
2018-07-05 22:12:33 +0800

    Add center based solution to longest  palindrome
    Signed-off-by: reminia <reminia.scarlet@gmail.com>

--- src/main/scala/LongestPalindrome.scala
+++ src/main/scala/LongestPalindrome.scala
@@ -19,7 +19,7 @@ object LongestPalindrome extends Test {
   def isPalindrome(s: String): Boolean = {
     @tailrec
     def f(i: Int): Boolean = {
-      if(i > s.length/2) true
+      if (i > s.length / 2) true
       else if (s(i) == s(s.length - i - 1)) f(i + 1)
       else false
     }
@@ -32,13 +32,48 @@ object LongestPalindrome extends Test {
     ???
   }
 
+  //center based validation
+  def v3(s: String): String = {
+    val length = s.length
+
+    @tailrec
+    def f(str: String, left: Int, right: Int): String = {
+      if (left >= 0 && right < length && s(left) == s(right)) {
+        f(s.substring(left, right + 1), left - 1, right + 1)
+      } else {
+        str
+      }
+    }
+
+    def odd(i: Int): String = {
+      f("", i, i)
+    }
+
+    def even(i: Int): String = {
+      f("", i, i + 1)
+    }
+
+    s.indices.foldLeft[String]("") {
+      case (res, i) =>
+        List(res, odd(i), even(i)).maxBy(_.length)
+    }
+
+  }
+
   def main(args: Array[String]): Unit = {
     longestPalindrome("aba") should be("aba")
     longestPalindrome("abcbd") should be("bcb")
     longestPalindrome("a") should be("a")
     longestPalindrome("") should be("")
+
     isPalindrome("aba") should be(true)
     isPalindrome("abc") should be(false)
     isPalindrome("a") should be(true)
+
+    v3("aba") should be("aba")
+    v3("abcbd") should be("bcb")
+    v3("a") should be("a")
+    v3("") should be("")
+
   }
 }
