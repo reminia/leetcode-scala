e457e359a0bd9d8fc42b002945b186f9ae2023af
reminia <reminia.scarlet@gmail.com>
2018-07-18 20:54:05 +0800

    Fix the atoi solution
    Signed-off-by: reminia <reminia.scarlet@gmail.com>

--- src/main/scala/Atoi.scala
+++ src/main/scala/Atoi.scala
@@ -2,19 +2,23 @@
 object Atoi extends Test {
 
   //WA => for input number may be larger than long
+  //AC after modification
   def atoi(str: String): Int = {
     def f(ss: String, sign: Int): Int = {
       if (ss.isEmpty) 0
       else if (!ss(0).isDigit) 0
       else {
-        val d = ss.takeWhile(_.isDigit)
-        val l = d.toLong
-        if (sign > 0) {
-          if (l > Int.MaxValue) Int.MaxValue
-          else l.toInt
-        } else {
-          if (-l < Int.MinValue) Int.MinValue
-          else -l.toInt
+        val d = ss.dropWhile(_ == '0').takeWhile(_.isDigit).take(11)
+        if (d.isEmpty) 0
+        else {
+          val l = d.toLong
+          if (sign > 0) {
+            if (l > Int.MaxValue) Int.MaxValue
+            else l.toInt
+          } else {
+            if (-l < Int.MinValue) Int.MinValue
+            else -l.toInt
+          }
         }
       }
     }
@@ -23,8 +27,8 @@ object Atoi extends Test {
     if (s.isEmpty) 0
     else {
       s(0) match {
-        case '+'             => f(s.drop(1), 1)
-        case '-'             => f(s.drop(1), -1)
+        case '+'             => f(s.tail, 1)
+        case '-'             => f(s.tail, -1)
         case a if !a.isDigit => 0
         case _               => f(s, 1)
       }
@@ -65,16 +69,27 @@ object Atoi extends Test {
     }
   }
 
+  // a fast traverse solution with mutable variables
+  def v3(str: String): Int = {
+    var i = 0
+    var begin = true
+    var sum = 0L
+    var isPositive = true
+    0
+  }
+
   def main(args: Array[String]): Unit = {
-//    atoi("a12") should be(0)
-//    atoi(" \t") should be(0)
-//    atoi("+") should be(0)
-//    atoi("-") should be(0)
-//    atoi(" +12abc") should be(12)
-//    atoi(" 12abc") should be(12)
-//    atoi(" -12abc") should be(-12)
-//    atoi("-91283472332") should be(Int.MinValue)
-//    atoi("2147483649") should be(Int.MaxValue)
+    atoi("a12") should be(0)
+    atoi(" \t") should be(0)
+    atoi("+") should be(0)
+    atoi("-") should be(0)
+    atoi(" +12abc") should be(12)
+    atoi(" 12abc") should be(12)
+    atoi(" -12abc") should be(-12)
+    atoi("-91283472332") should be(Int.MinValue)
+    atoi("2147483649") should be(Int.MaxValue)
+    atoi("000123") should be(123)
+    atoi(" 00000 ") should be(0)
 
     v2("a12") should be(0)
     v2(" \t") should be(0)
@@ -86,6 +101,7 @@ object Atoi extends Test {
     v2("-91283472332") should be(Int.MinValue)
     v2("2147483649") should be(Int.MaxValue)
     v2("000123") should be(123)
+    v2(" 00000 ") should be(0)
   }
 
 }
