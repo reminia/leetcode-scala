56745507a7e51a4b3688d87b2a354add12e89112
reminia <reminia.scarlet@gmail.com>
2018-07-18 21:52:42 +0800

    Add solution based on number accumulation
    Signed-off-by: reminia <reminia.scarlet@gmail.com>

--- src/main/scala/PalindromeNumber.scala
+++ src/main/scala/PalindromeNumber.scala
@@ -15,6 +15,32 @@ object PalindromeNumber extends Test {
     f(0)
   }
 
+  def v3(x: Int): Boolean = {
+    if (x < 0 || x % 10 == 0 && x != 0) false
+    else {
+      var y = x
+      var res = 0
+      while (y != 0) {
+        res = res * 10 + y % 10
+        y = y / 10
+      }
+      res == x
+    }
+  }
+
+  def v4(x: Int): Boolean = {
+    if (x < 0 || x % 10 == 0 && x != 0) false
+    else {
+      var y = x
+      var res = 0
+      while (y > res) {
+        res = res * 10 + y % 10
+        y = y / 10
+      }
+      res == y || res / 10 == y
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     v1(1) should be(true)
     v1(11) should be(true)
@@ -25,6 +51,22 @@ object PalindromeNumber extends Test {
     v2(11) should be(true)
     v2(12) should be(false)
     v2(123) should be(false)
+
+    v3(-1) should be(false)
+    v3(10) should be(false)
+    v3(1) should be(true)
+    v3(11) should be(true)
+    v3(12) should be(false)
+    v3(121) should be(true)
+    v3(123) should be(false)
+
+    v4(-1) should be(false)
+    v4(10) should be(false)
+    v4(1) should be(true)
+    v4(11) should be(true)
+    v4(12) should be(false)
+    v4(121) should be(true)
+    v4(123) should be(false)
   }
 
 }
