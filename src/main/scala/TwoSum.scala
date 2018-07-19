45292a51bc4c7577e39136143d3e59126350b88a
reminia <reminia.scarlet@gmail.com>
2018-07-19 23:20:42 +0800

    Add hash based solution of 2sum
    Signed-off-by: reminia <reminia.scarlet@gmail.com>

--- src/main/scala/TwoSum.scala
+++ src/main/scala/TwoSum.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 object TwoSum extends Test {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
     val array = for {
@@ -9,7 +11,40 @@ object TwoSum extends Test {
     Array(a, b)
   }
 
+  // two pass hash table
+  def v2(nums: Array[Int], target: Int): Array[Int] = {
+    val map = nums.zipWithIndex.toMap
+    nums.zipWithIndex.view.collect {
+      new PartialFunction[(Int, Int), Array[Int]] {
+        override def isDefinedAt(x: (Int, Int)): Boolean = {
+          val remain = target - x._1
+          val index = map.getOrElse(remain, -1)
+          index != -1 && index != x._2
+        }
+
+        override def apply(v1: (Int, Int)): Array[Int] =
+          Array(v1._2, map(target - v1._1))
+      }
+    }.head
+  }
+
+  // one pass hash table
+  def v3(nums: Array[Int], target: Int): Array[Int] = {
+    val map: mutable.Map[Int, Int] = mutable.Map.empty
+    for ((v, i) <- nums.zipWithIndex) {
+      val r = target - v
+      if (map.contains(r))
+        return Array(i, map(r))
+      else map += v -> i
+    }
+    Array(0, 0)
+  }
+
   def main(args: Array[String]): Unit = {
     twoSum(Array(2, 2, 1, 4), 4) shouldEqual Array(0, 1)
+    twoSum(Array(2, 2, 1, 4), 3) shouldEqual Array(0, 2)
+
+    v2(Array(2, 2, 1, 4), 4) shouldEqual Array(0, 1)
+    v2(Array(2, 2, 1, 4), 3) shouldEqual Array(0, 2)
   }
 }
