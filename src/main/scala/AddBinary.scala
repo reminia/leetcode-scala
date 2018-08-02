a2f4b13c9574da3a012aedc62ccf91bb64c855c5
reminia <reminia.scarlet@gmail.com>
2018-08-02 20:39:48 +0800

    Add AC solution
    Signed-off-by: reminia <reminia.scarlet@gmail.com>

--- src/main/scala/AddBinary.scala
+++ src/main/scala/AddBinary.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 object AddBinary extends Test {
   def value(s: String): Long = {
     var sum = 0L
@@ -12,10 +14,10 @@ object AddBinary extends Test {
     val sb = new StringBuilder
     var n = s
     while (n / 2 != 0) {
-      sb.append(n%2)
+      sb.append(n % 2)
       n = n / 2
     }
-    sb.append(n%2)
+    sb.append(n % 2)
     sb.toString.reverse
   }
 
@@ -24,6 +26,41 @@ object AddBinary extends Test {
   def addBinary(a: String, b: String): String = {
     val sum = value(a) + value(b)
     binary(sum)
+  }
+
+  //emulate plus of binary numbers
+  def v2(c: String, d: String): String = {
+    var res: mutable.ListBuffer[Char] = mutable.ListBuffer.empty
+    var i = 0
+    var flag = 0
+    val a = c.reverse
+    val b = d.reverse
+    while (i < a.length || i < b.length) {
+      var sum = flag
+      if (i < a.length) {
+        sum += a(i).toString.toInt
+      }
+      if (i < b.length) {
+        sum += b(i).toString.toInt
+      }
+      if (sum == 2) {
+        '0' +=: res
+        flag = 1
+      } else if (sum == 3) {
+        '1' +=: res
+        flag = 1
+      } else if (sum == 1) {
+        '1' +=: res
+        flag = 0
+      } else {
+        '0' +=: res
+        flag = 0
+      }
+      i += 1
+    }
+    if (flag == 1)
+      '1' +=: res
+    res.mkString
   }
 
   def main(args: Array[String]): Unit = {
@@ -35,5 +72,8 @@ object AddBinary extends Test {
     addBinary("1", "1") should be("10")
     addBinary("10", "1") should be("11")
     addBinary("10", "10") should be("100")
+    v2("1", "1") should be("10")
+    v2("10", "1") should be("11")
+    v2("10", "10") should be("100")
   }
 }
